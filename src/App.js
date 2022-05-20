9a93fe19b08fa300d7d7460eb23ddf2e962050b8
Cannon-Cole <colecannonschool@gmail.com>
2022-05-19 23:03:14 -0600

    Initialize notes state using a function

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Split from "react-split";
 import { nanoid } from "nanoid";
 
 export default function App() {
-  const [notes, setNotes] = React.useState(get_locally() || []);
+  const [notes, setNotes] = React.useState(() => get_locally() || []);
   const [currentNoteId, setCurrentNoteId] = React.useState(
     (notes[0] && notes[0].id) || ""
   );
